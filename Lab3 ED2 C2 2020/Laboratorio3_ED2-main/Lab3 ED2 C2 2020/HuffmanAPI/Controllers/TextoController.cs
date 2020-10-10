df651e2ad9399dde24bec89fdc32699f502b4cab
diegosalazar00 <57643277+diegosalazar00@users.noreply.github.com>
2020-10-09 23:48:18 -0600

    API

---
+++ Lab3 ED2 C2 2020/Laboratorio3_ED2-main/Lab3 ED2 C2 2020/HuffmanAPI/Controllers/TextoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Threading.Tasks;
+using Lab3_ED2_C2_2020;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HuffmanAPI.Controllers
+{
+    [Route("api/compress")]
+    [ApiController]
+    public class TextoController : ControllerBase
+    {
+        [HttpPost]
+        [Route("{name}")]
+        public IComparable<string> Post(string name)
+        {
+            Huffman text = new Huffman();
+            return text.nombrearchivooperado;
+        }
+    }
+    [HttpPost("api/descompress")]
+    
+    
+        
+    
+}
---
+++ Lab3 ED2 C2 2020/Laboratorio3_ED2-main/Lab3 ED2 C2 2020/HuffmanAPI/Models/Archivo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab3_ED2_C2_2020;
+
+namespace HuffmanAPI.Models
+{
+    public class Archivo
+    {
+        Huffman huffman1 = new Huffman();
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
